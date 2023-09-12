2ab436cc6b49c3bccbd98d10128e32ca76a51380
91766 <91766@ROHITH.bbrouter>
2023-09-12 15:52:07 +0530

    okk

---
+++ src/test/java/Demo/Git.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class Git {
+
+	public static void main(String[] args) {
+    System.out.println("hii i am coming dont worry ");
+
+	}
+
+}
--- src/test/java/Demo/Sample.java
+++ src/test/java/Demo/Sample.java
@@ -3,7 +3,7 @@ package Demo;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("hi this is my first commit");
+		System.out.println("Hi this is my first commit");
 
 	}
 
